0f82f8b58c729c79823caef8525746006a4c3228
Kyler Johnson <kylerjohnsondev@gmail.com>
2022-02-12 13:33:09 -0500

    add state base

---
+++ src/app/global-state/state-base.ts
@@ -0,0 +1,65 @@
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+/*
+ * @class {StateService<T> StateService}
+ */
+export abstract class StateService<T> {
+  private stateSubject$ = new BehaviorSubject<T | null>(null);
+
+  /*
+   * @property {Observable<T>} state$ - state observable
+   */
+  state$: Observable<T | null> = this.stateSubject$.asObservable();
+
+  /*
+   *  @function pluckStateProperty
+   *  @description returns a property of the state object in an observable stream
+   *  @param {K} propertyName
+   *  @returns {Observable<T[K]>}
+   */
+  pluckStateProperty<K extends keyof T>(
+    propertyName: K
+  ): Observable<T[K] | null> {
+    return this.state$.pipe(
+      filter((state) => !!state),
+      map((state) => {
+        return state?.[propertyName] ?? null;
+      })
+    );
+  }
+
+  /*
+   *  @function setState
+   *  @description Updates state
+   *  @param {(T|Partial<T>)} state
+   */
+  protected setState(state: T | Partial<T>): void {
+    const currentState = this.stateSubject$.value;
+    const newState = { ...currentState, ...state } as T;
+    this.stateSubject$.next(newState);
+  }
+
+  /*
+   *  @function getStateCopy
+   *  @description immutably gets copy of current state value
+   *  @returns {T}
+   */
+  protected getStateCopy(): T | null {
+    const currentState = { ...this.stateSubject$.value };
+    if (currentState) return currentState as T;
+    return null;
+  }
+
+  /*
+   *  @function resetState
+   *  @description resets state to null
+   */
+  protected resetState(): void {
+    this.stateSubject$.next(null);
+  }
+
+  protected endStateObservation(): void {
+    this.stateSubject$.complete();
+  }
+}
